9bd49dcf6a0663c38c5306bd66d50f437794de18
Damian Karzon <dkarzon@tboda.com>
2013-09-01 17:32:07 +1200

    Added the WinJS sample app

---
+++ WhatsNewInWindows81/CatHubSample/js/data.js
@@ -0,0 +1,124 @@
+﻿(function () {
+    "use strict";
+
+    var list = new WinJS.Binding.List();
+    var groupedItems = list.createGrouped(
+        function groupKeySelector(item) { return item.group.key; },
+        function groupDataSelector(item) { return item.group; }
+    );
+
+    // TODO: Replace the data with your real data.
+    // You can add data from asynchronous sources whenever it becomes available.
+    generateSampleData().forEach(function (item) {
+        list.push(item);
+    });
+
+    WinJS.Namespace.define("Data", {
+        items: groupedItems,
+        groups: groupedItems.groups,
+        getItemReference: getItemReference,
+        getItemsFromGroup: getItemsFromGroup,
+        resolveGroupReference: resolveGroupReference,
+        resolveItemReference: resolveItemReference
+    });
+
+    // Get a reference for an item, using the group key and item title as a
+    // unique reference to the item that can be easily serialized.
+    function getItemReference(item) {
+        return [item.group.key, item.title];
+    }
+
+    // This function returns a WinJS.Binding.List containing only the items
+    // that belong to the provided group.
+    function getItemsFromGroup(group) {
+        return list.createFiltered(function (item) { return item.group.key === group.key; });
+    }
+
+    // Get the unique group corresponding to the provided group key.
+    function resolveGroupReference(key) {
+        return groupedItems.groups.getItemFromKey(key).data;
+    }
+
+    // Get a unique item from the provided string array, which should contain a
+    // group key and an item title.
+    function resolveItemReference(reference) {
+        for (var i = 0; i < groupedItems.length; i++) {
+            var item = groupedItems.getAt(i);
+            if (item.group.key === reference[0] && item.title === reference[1]) {
+                return item;
+            }
+        }
+    }
+
+    // Returns an array of sample data that can be added to the application's
+    // data list. 
+    function generateSampleData() {
+        var itemContent = "Cat ipsum dolor sit amet, inspect anything brought into the house. Play time hate dog. Sweet beast play time rub face on everything lick butt for hunt anything that moves. Swat at dog hunt anything that moves but sweet beast but hopped up on goofballs intently stare at the same spot. Missing until dinner time stare at ceiling yet burrow under covers so leave dead animals as gifts";
+        var itemDescription = "Stand in front of the computer screen mark territory all of a sudden go crazy swat at dog leave dead animals as gifts.";
+        var groupDescription = "Group Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempor scelerisque lorem in vehicula. Aliquam tincidunt, lacus ut sagittis tristique, turpis massa volutpat augue, eu rutrum ligula ante a ante";
+
+        // These three strings encode placeholder images. You will want to set the
+        // backgroundImage property in your real data to be URLs to images.
+        var cat1 = "/images/cats/cat1.jpg";
+        var cat2 = "/images/cats/cat2.jpg";
+        var cat3 = "/images/cats/cat3.jpg";
+        var cat4 = "/images/cats/cat4.jpg";
+        var cat5 = "/images/cats/cat5.jpg";
+
+        // Each of these sample groups must have a unique key to be displayed
+        // separately.
+        var sampleGroups = [
+            { key: "group1", title: "Group Title: 1", subtitle: "Group Subtitle: 1", backgroundImage: cat1, description: groupDescription },
+            { key: "group2", title: "Group Title: 2", subtitle: "Group Subtitle: 2", backgroundImage: cat2, description: groupDescription },
+            { key: "group3", title: "Group Title: 3", subtitle: "Group Subtitle: 3", backgroundImage: cat3, description: groupDescription },
+            { key: "group4", title: "Group Title: 4", subtitle: "Group Subtitle: 4", backgroundImage: cat2, description: groupDescription },
+            { key: "group5", title: "Group Title: 5", subtitle: "Group Subtitle: 5", backgroundImage: cat3, description: groupDescription },
+            { key: "group6", title: "Group Title: 6", subtitle: "Group Subtitle: 6", backgroundImage: cat1, description: groupDescription }
+        ];
+
+        // Each of these sample items should have a reference to a particular
+        // group.
+        var sampleItems = [
+            { group: sampleGroups[0], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+            { group: sampleGroups[0], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: cat1 },
+            { group: sampleGroups[0], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: cat3 },
+            { group: sampleGroups[0], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: cat5 },
+            { group: sampleGroups[0], title: "Item Title: 5", subtitle: "Item Subtitle: 5", description: itemDescription, content: itemContent, backgroundImage: cat3 },
+
+            { group: sampleGroups[1], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: cat1 },
+            { group: sampleGroups[1], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: cat3 },
+            { group: sampleGroups[1], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+
+            { group: sampleGroups[2], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: cat4 },
+            { group: sampleGroups[2], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+            { group: sampleGroups[2], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: cat1 },
+            { group: sampleGroups[2], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+            { group: sampleGroups[2], title: "Item Title: 5", subtitle: "Item Subtitle: 5", description: itemDescription, content: itemContent, backgroundImage: cat3 },
+            { group: sampleGroups[2], title: "Item Title: 6", subtitle: "Item Subtitle: 6", description: itemDescription, content: itemContent, backgroundImage: cat1 },
+            { group: sampleGroups[2], title: "Item Title: 7", subtitle: "Item Subtitle: 7", description: itemDescription, content: itemContent, backgroundImage: cat3 },
+
+            { group: sampleGroups[3], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: cat1 },
+            { group: sampleGroups[3], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+            { group: sampleGroups[3], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: cat4 },
+            { group: sampleGroups[3], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+            { group: sampleGroups[3], title: "Item Title: 5", subtitle: "Item Subtitle: 5", description: itemDescription, content: itemContent, backgroundImage: cat3 },
+            { group: sampleGroups[3], title: "Item Title: 6", subtitle: "Item Subtitle: 6", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+
+            { group: sampleGroups[4], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+            { group: sampleGroups[4], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: cat1 },
+            { group: sampleGroups[4], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: cat5 },
+            { group: sampleGroups[4], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: cat3 },
+
+            { group: sampleGroups[5], title: "Item Title: 1", subtitle: "Item Subtitle: 1", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+            { group: sampleGroups[5], title: "Item Title: 2", subtitle: "Item Subtitle: 2", description: itemDescription, content: itemContent, backgroundImage: cat1 },
+            { group: sampleGroups[5], title: "Item Title: 3", subtitle: "Item Subtitle: 3", description: itemDescription, content: itemContent, backgroundImage: cat4 },
+            { group: sampleGroups[5], title: "Item Title: 4", subtitle: "Item Subtitle: 4", description: itemDescription, content: itemContent, backgroundImage: cat1 },
+            { group: sampleGroups[5], title: "Item Title: 5", subtitle: "Item Subtitle: 5", description: itemDescription, content: itemContent, backgroundImage: cat2 },
+            { group: sampleGroups[5], title: "Item Title: 6", subtitle: "Item Subtitle: 6", description: itemDescription, content: itemContent, backgroundImage: cat3 },
+            { group: sampleGroups[5], title: "Item Title: 7", subtitle: "Item Subtitle: 7", description: itemDescription, content: itemContent, backgroundImage: cat1 },
+            { group: sampleGroups[5], title: "Item Title: 8", subtitle: "Item Subtitle: 8", description: itemDescription, content: itemContent, backgroundImage: cat5 }
+        ];
+
+        return sampleItems;
+    }
+})();
---
+++ WhatsNewInWindows81/CatHubSample/pages/hub/hub.js
@@ -0,0 +1,108 @@
+﻿(function () {
+    "use strict";
+
+    var nav = WinJS.Navigation;
+    var session = WinJS.Application.sessionState;
+    var util = WinJS.Utilities;
+
+    // Get the groups used by the data-bound sections of the Hub.
+    var section2Group = Data.resolveGroupReference("group1");
+    var section5Group = Data.resolveGroupReference("group6");
+
+    WinJS.UI.Pages.define("/pages/hub/hub.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            var hub = element.querySelector(".hub").winControl;
+            hub.onloadingstatechanged = function (args) {
+                if (args.srcElement === hub.element && args.detail.loadingState === "complete") {
+                    this._hubReady(hub);
+                    hub.onloadingstatechanged = null;
+                }
+            }.bind(this);
+
+            hub.onheaderinvoked = function (args) {
+                args.detail.section.onheaderinvoked(args);
+            };
+
+            // TODO: Initialize the page here.
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+            //session.hubScroll = document.querySelector(".hub").winControl.scrollPosition;
+        },
+
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in layout.
+        },
+
+        _hubReady: function (hub) {
+            /// <param name="hub" type="WinJS.UI.Hub" />
+
+            WinJS.Resources.processAll();
+            //if (typeof session.hubScroll === "number") {
+            //    hub.scrollPosition = session.hubScroll;
+            //}
+
+            // TODO: Initialize the hub sections here.
+        },
+    });
+
+    function createHeaderNavigator(group) {
+        return util.markSupportedForProcessing(function (args) {
+            nav.navigate("/pages/section/section.html", { title: this.header, groupKey: group.key });
+        });
+    }
+
+    function createItemNavigator(group) {
+        var items = Data.getItemsFromGroup(group);
+        return util.markSupportedForProcessing(function (args) {
+            var item = Data.getItemReference(items.getAt(args.detail.itemIndex));
+            nav.navigate("/pages/item/item.html", { item: item });
+        });
+    }
+
+    function getItemsDataSourceFromGroup(group) {
+        return Data.getItemsFromGroup(group).dataSource;
+    }
+
+
+    //Handle SearchBox interactions here
+
+    function searchBoxSuggestionsRequested() {
+        return util.markSupportedForProcessing(function (eventInfo) {
+            //Do something with the search query to bring back suggestions
+            var searchQuery = eventInfo.detail.queryText;
+            var searchLanguage = eventInfo.detail.language;
+
+
+            //Populate suggestions here
+            eventInfo.detail.searchSuggestionCollection.appendQuerySuggestion("Swat at dog");
+            eventInfo.detail.searchSuggestionCollection.appendQuerySuggestion("Stand in front of the computer screen");
+
+
+            //or you can append an array of suggestions
+            eventInfo.detail.searchSuggestionCollection.appendQuerySuggestions(["rub face on everything", "intrigued by the shower"]);
+
+
+            //or you can append actual result data
+            //NOTE: you must handle the resultsuggestionchosen event for selection of these results
+            var imageUri = new Windows.Foundation.Uri("ms-appx:///images/cats/section1.png");
+            var imageSource = Windows.Storage.Streams.RandomAccessStreamReference.createFromUri(imageUri);
+            eventInfo.detail.searchSuggestionCollection.appendResultSuggestion("Cat page", "View all the cat page", "tag", imageSource, "image alt text");
+        });
+    };
+
+
+    WinJS.Namespace.define("HubPage", {
+        section2DataSource: getItemsDataSourceFromGroup(section2Group),
+        section2HeaderNavigate: createHeaderNavigator(section2Group),
+        section2ItemNavigate: createItemNavigator(section2Group),
+        section5DataSource: getItemsDataSourceFromGroup(section5Group),
+        section5ItemNavigate: createItemNavigator(section5Group),
+        searchBoxSuggestionsRequested: searchBoxSuggestionsRequested()
+    });
+})();
